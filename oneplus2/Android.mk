f8489d9d5352551f6783712fa8a45de0dc957b00
Willi Ye <williye97@gmail.com>
2015-12-26 22:55:19 +0100

    oneplus2: Import blobs from OnePlus2Oxygen_14_OTA_011_all_1512111634
    Conflicts:
    	oneplus2/oneplus2-vendor-blobs.mk
    	oneplus2/proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -17,15 +17,15 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),oneplus2)
-ifeq ($(QCPATH),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
@@ -307,6 +307,5 @@ include $(BUILD_PREBUILT)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
-endif
 endif
 
